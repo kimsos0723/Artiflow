bee492dd636621b2f10e622d9337dd850c34c692
xylitol <kimsos0723@gmail.com>
2019-10-23 22:16:31 +0900

    Add Ui

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -7,7 +7,6 @@ from PyQt5 import uic
 
 from Views import Ui_MainWindow as Ui
 # form_class = uic.loadUiType('./Views/MainWindow.ui')[0]
-
 class WindowClass(QMainWindow, Ui.Ui_MainWindow):    
     """
     Main Window Class
@@ -18,61 +17,43 @@ class WindowClass(QMainWindow, Ui.Ui_MainWindow):
     def __init__(self):
         super().__init__()        
         self.setupUi(self)
-        self.initMenu(self)    
+        self.initUi(self)
         
-        
-    def initMenu(self,_) :
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('&File')
-        helpMenu = mainMenu.addMenu('&Help')
-        
-        #File menucle
-        AddFile = QAction('&Add File',self)
-        AddFile.triggered.connect(self.AddFileAction)
-        #Help menu
-        fileMenu.addAction(AddFile)
-        #TreeWidget Header
-        self.tableWidget.setVerticalHeaderLabels(["Databases"])
-        
-        
-    def AddFileAction(self, _):
-        self.openFileNameDialog(self)
-        self.addTreeWidgetItems(self)
+    def initUi(self, _):
+        self.initManu(self)
 
-    def openFileNameDialog(self,_) :
-        options = QFileDialog.Options()    
+    def initManu(self, _):     
+        mainManu = self.menuBar()
+        fileManu = mainManu.addMenu('&File')
+        viewManu = mainManu.addMenu('&View')
+        helpManu = mainManu.addMenu('&Help')
+
+        #file menu
+        AddFile = QAction('&Add File',self)
+        AddFile.triggered.connect(self.AddfileAction)
+        fileManu.addAction(AddFile)
+        
+    def AddfileAction(self, _):
+        path = ""
+        path = self.openFileNameDialog(self)    
+        if path == "" :
+            return
+        
+        self.tabWidget.addTab(QTreeView(), self.DBfilePaths[-1].split('/')[-1])
+
+    def openFileNameDialog(self,_):
+        options = QFileDialog.Options()
         path, _ = QFileDialog.getOpenFileName(self,"Open DB File", "", "DB Files (*.db);;All Files (*)",options=options)
-        if path:            
+
+        try:          
             msg = QMessageBox()
             msg.setText(path)
             msg.exec_()
-            self.DBfilePaths.append(path)              
-        else :
-            msg = QMessageBox()
-            msg.setText("Cannot Open !")
-            msg.exec_()
-    
-    def addTreeWidgetItems(self, _) :
-        db_ctr = DBController(self.DBfilePaths[0])
-        #Get Tables
-        _, result = db_ctr.excute_sql('SELECT name from sqlite_master where type= "table"')
-        self.treeWidget.setColumnCount(len(self.DBfilePaths))
-        root_item = QTreeWidgetItem([self.DBfilePaths[0].split('/')[-1]])
-        self.treeWidget.addTopLevelItem(root_item)
-        for i in result:
-            QTreeWidgetItem(root_item, list(i))
-                        
-    def loadTable(self, _):
-        try:
-            names, result = db_ctr.excute_sql()
+            self.DBfilePaths.append(path) 
         except Exception as e:
-            print(str(e))
-            return            
-
-        self.tableWidget.setRowCount(0)            
-        self.tableWidget.setColumnCount(len(names))                
-        for row_num, row_data in enumerate(result):
-            self.tableWidget.insertRow(row_num)
-            for column_num, data in enumerate(row_data):
-                self.tableWidget.setItem(row_num, column_num, QTableWidgetItem(str(data)))
-      
+            msg = QMessageBox()
+            msg.setText("Cannot Open !")            
+            msg.exec_()
+        
+    def addTreeView(self, _):
+           
